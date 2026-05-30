4929d1398052f21fa4e9a64cbd819f034990778d
TOBBE twizzaq <AIKtq@proton.me>
2026-05-30 09:16:08 +0200

    Update bot.js
    color change

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#f9f209", 
+      secondary: "#2986cc", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
